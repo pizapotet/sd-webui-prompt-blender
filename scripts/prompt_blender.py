f057781b29fa952cd5b85d60dcded51d5118c884
pizapotet <pizapotet009@gmail.com>
2023-11-23 11:38:00 +0900

    temporarily place the button

--- scripts/prompt_blender.py
+++ scripts/prompt_blender.py
@@ -1,6 +1,22 @@
 import gradio as gr
+import modules
 
 from modules import script_callbacks
+
+
+def send_to_buttons_component(image_component, output_result_component):
+    try:
+        send_to_buttons = modules.generation_parameters_copypaste.create_buttons(
+            ["txt2img", "img2img", "inpaint", "extras"])
+        for tabname, button in send_to_buttons.items():
+            print(tabname)
+            params = ParamBinding(paste_button=button, tabname=tabname, source_text_component=output_result_component,
+                                  source_image_component=image_component, source_tabname=f"{tabname}_source_tabname")
+            modules.generation_parameters_copypaste.register_paste_params_button(
+                params)
+        return send_to_buttons
+    except:
+        pass
 
 
 def on_ui_tabs():
@@ -12,6 +28,11 @@ def on_ui_tabs():
                     output_result_component = gr.Textbox(
                         label="Prompt", interactive=False
                     )
+                    hidden_image_component = gr.Image(
+                        type="pil", elem_id="prompt_blender_hidden_image")
+                    with gr.Row():
+                        send_to_buttons_component(hidden_image_component,
+                                                  output_result_component)
                 with gr.Column():
                     # TODO: Input tags
                     gr.Markdown(
