496812321199868de059995804519e5e21cbe1cf
pizapotet <pizapotet009@gmail.com>
2023-11-25 14:33:34 +0900

    add input textbox

--- scripts/prompt_blender.py
+++ scripts/prompt_blender.py
@@ -25,6 +25,14 @@ def build_prompt():
     return textwrap.dedent(tmp_value)[1:-1]
 
 
+def tag_component(elem_id):
+    with gr.Row():
+        gr.Textbox(
+            show_label=False, interactive=True, elem_id=elem_id
+        )
+    pass
+
+
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
         with gr.Tab("Blend"):
@@ -38,12 +46,8 @@ def on_ui_tabs():
                               _js="(str) => onClickGenerate(str)")
                 with gr.Column():
                     # TODO: Input tags
-                    gr.Markdown(
-                        """
-                        # Hello World!
-                        Start typing below to see the output.
-                        """
-                    )
+                    for index in range(5):
+                        tag_component(elem_id=f"prompt_blender_input_{index}")
         with gr.Tab("Management"):
             with gr.Row():
                 # TODO: Edit and Serch tags
